b662ee8c442579938f16d740bb50cc2a671c5805
Chiemerie <alexemerie7@gmail.com>
2024-11-25 18:25:51 +0000

    Implemented user authentication routes and model

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
     res.send('Backend is running');
 });
