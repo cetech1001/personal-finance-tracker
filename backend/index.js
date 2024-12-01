f6240b14a78b211be1d470f5928b4c875b26b429
Chiemerie <alexemerie7@gmail.com>
2024-12-01 18:31:19 +0000

    Changed ports on the apps

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
